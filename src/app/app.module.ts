5cb985b9ef46b5edb7856e2f3516e25e5e046732
Vivixing <echeverri.vivian1120@gmail.com>
2024-05-26 18:28:53 -0500

    Corrección componente resultado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    ResultadoComponent
   ],
   imports: [
     BrowserModule,
