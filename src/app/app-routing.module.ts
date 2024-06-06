9e119325ea1874bb98ee686dc495eea91cd5e56a
Vivixing <echeverri.vivian1120@gmail.com>
2024-06-06 14:40:22 -0500

    Agregado apartado de los paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { GameComponent } from './game/game.component';
+import { ResultadoComponent } from './resultado/resultado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'Index', pathMatch:'full'},
+  {path:'Index', component: IndexComponent},
+  {path:'Game', component:GameComponent},
+  {path:'Result', component:ResultadoComponent},
+  {path:'**', redirectTo:'Index', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
