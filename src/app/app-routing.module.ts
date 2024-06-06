715bc41985045d53d0d5bfda098384e042331a6e
Vivixing <echeverri.vivian1120@gmail.com>
2024-06-06 17:32:45 -0500

    Agregado ruta de Salas Disponibles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { GameComponent } from './game/game.component';
 import { ResultadoComponent } from './resultado/resultado.component';
+import { SalasDisponiblesComponent } from './salas-disponibles/salas-disponibles.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'Index', pathMatch:'full'},
   {path:'Index', component: IndexComponent},
   {path:'Game', component:GameComponent},
   {path:'Result', component:ResultadoComponent},
+  {path:'SalasDisponibles', component:SalasDisponiblesComponent},
   {path:'**', redirectTo:'Index', pathMatch:'full'}
 ];
 
