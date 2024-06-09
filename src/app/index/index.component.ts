bcc22e22dff3cf7caaa1c43cf7c9b9ab27756492
Vivixing <echeverri.vivian1120@gmail.com>
2024-06-09 18:13:22 -0500

    Creado lógica del apartado editar avatar

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  modalOpen = false;
+  avatarSelected = {name:'avatarDefault', src:'media/usuario.png'}
+  avatarList = [
+    {name:'avatar1', src:'media/avatar1.svg'},
+    {name:'avatar2', src:'media/avatar2.svg'},
+    {name:'avatar3', src:'media/avatar3.svg'},
+    {name:'avatar4', src:'media/avatar4.svg'},
+    {name:'avatar5', src:'media/avatar5.svg'},
+    {name:'avatar6', src:'media/avatar6.svg'}
+  ]
+  ngOnInit(): void {
+    
+  }
 
+  changeAvatar(){
+    this.modalOpen = true;
+  }
+
+  closeAvatarSelection(){
+    this.modalOpen = false;
+  }
+
+  selectAvatar(avatar:any){
+    this.avatarSelected = avatar
+    this.modalOpen = false;
+  }
 }
